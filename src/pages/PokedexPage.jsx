8269cd7bd86edc7d56aa67a3051797279bc4a906
Diego Castro <diegorbcastro@gmail.com>
2024-01-26 03:17:02 -0400

    Pokedex_ent5_gen33

--- src/pages/PokedexPage.jsx
+++ src/pages/PokedexPage.jsx
@@ -5,11 +5,19 @@ import PokeCard from "../Components/PokedexPage/PokeCard";
 import SelectType from "../Components/PokedexPage/SelectType";
 import "./stylesMain/PokedexPage.css";
 import pokedex from "../assets/img pokedex.png"
+import Pagination from "../Components/PokedexPage/Pagination";
 
 const PokedexPage = () => {
   const trainerName = useSelector((states) => states.trainer);
   const [inputValue, setInputValue] = useState("");
   const [typeSelected, setTypeSelected] = useState("allPokemons"); //typeSelected es la url del tipo que esra en el archico selecType en value
+  const [pokemonPerPage, setpokemonPerPage] = useState(100) //en este estado dire cuantos pokemones quiero por pagina
+  const [currentPage, setCurrentPage] = useState(1) //pagina actual
+
+  const lastIndex= currentPage * pokemonPerPage
+  const firstIndex = lastIndex - pokemonPerPage
+ 
+  
 
   const url = "https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0";
 
@@ -23,9 +31,12 @@ const PokedexPage = () => {
     }
   }, [typeSelected]);
 
-  // console.log(pokemons)
+  const totalPokemons=pokemons?.results.length  //cantidadd de pokemones
 
-  console.log(typeSelected);
+  console.log(pokemons?.results)
+  console.log(totalPokemons);
+
+  // console.log(typeSelected);
 
   const inputName = useRef();
 
@@ -57,10 +68,13 @@ const PokedexPage = () => {
           <SelectType  setTypeSelected={setTypeSelected} />
         </form>
         </aside>
+
+        <Pagination pokemonPerPage={pokemonPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage} totalPokemons={totalPokemons}/>
+
         <div className="container_pokecard">
           {pokemons?.results.filter(cbFilter).map((pokeInfo) => (
             <PokeCard key={pokeInfo.url} url={pokeInfo.url} />
-          ))}
+          )).slice(firstIndex, lastIndex)}
         </div>
       </div>
     </div>
